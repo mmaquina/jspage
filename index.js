8101d4e81913e4e34e1301e004c55bf000798676
mmaquina <mmaquina@gmail.com>
2023-03-17 21:11:35 -0300

    showing pics

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const projects = [
         "Second point",
         "Third item"
       ],
-      image: "image_url"
+      image: "https://picsum.photos/id/235/300/150"
     },
     {
       title: "Project name 2",
@@ -19,7 +19,95 @@ const projects = [
         "Second point",
         "Third item"
       ],
-      image: "image_url"
+      image: "https://picsum.photos/id/237/300/150"
+    },
+    {
+      title: "Project name 3",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/227/300/150"
+    },
+    {
+      title: "Project name 4",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/273/300/150"
+    },
+    {
+      title: "Project name 5",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/271/300/150"
+    },
+    {
+      title: "Project name 6",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/247/300/150"
+    },
+    {
+      title: "Project name 7",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/277/300/150"
+    },
+    {
+      title: "Project name 8",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/242/300/150"
+    },
+    {
+      title: "Project name 9",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/241/300/150"
+    },
+    {
+      title: "Project name 10",
+      description:
+        "A cool description",
+      details: [
+        "First detail",
+        "Second point",
+        "Third item"
+      ],
+      image: "https://picsum.photos/id/240/300/150"
     }
 ];
 
@@ -32,4 +120,6 @@ projects.forEach(project => {
         container.innerHTML +=  `<li>${detail}</li>`;
     });
     container.innerHTML += `</ul>`;
+    container.innerHTML += `<img src="${project.image}" alt="pic">`;
+    container.innerHTML += `<p>${project.description}</p>`;
 });
